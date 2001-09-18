4f9f8eac284df53cf6635f6fe5cade835cc04cd7
vishnu <vishnu>
2001-09-18 18:42:03 +0000

    Initial revision

---
+++ makeweb/art/download.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+web=/home/groups/redael
+
+scp vishnu@shell.berlios.de:$web/htdocs/art/* .
---
+++ makeweb/art/upload.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+web=/home/groups/redael
+
+scp *.png vishnu@shell.berlios.de:$web/htdocs/art/
